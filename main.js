0f2a382e9d54091b9bf994396a96248c99a21301
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-01-26 18:24:43 +0000

    added js for switch

---
+++ main.js
@@ -0,0 +1,9 @@
+function changeTheme() {
+    var scheme = document.getElementById("theme").value;
+
+    if (document.getElementById("theme").checked) {
+        console.log("working")
+    } else {
+        console.log("hi")
+    }
+}
