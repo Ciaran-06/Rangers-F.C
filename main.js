20952871a596432a388c9dbbd5806dadc617eb87
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-01-26 19:02:03 +0000

    wokring on navbar

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ function changeTheme() {
         elements.classList.toggle("dark-mode");
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
